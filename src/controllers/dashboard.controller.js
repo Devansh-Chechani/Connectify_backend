7cc30686b77aa1bdd872df588d03d4f8a227aaea
Devansh-Chechani <113038123+Devansh-Chechani@users.noreply.github.com>
2024-05-03 15:04:39 +0530

    Update dashboard.controller.js

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -11,30 +11,71 @@ const getChannelStats = asyncHandler(async (req, res) => {
        const videos = await Video.find({owner:req.user?._id})
        const subscribers = await Subscription.find({channel:req.user._id})
        const likes = await Like.find({likedBy:req.user._id})
+       let views = 0;
+       videos.map((video)=>{
+         views+= video.views
+       })
    
-       const stats = {
-          videos,
-          subscribers,
-          likes
+       const dashboard = {
+         totalVideos:videos.length,
+         totalSubscribers: subscribers.length,
+         totalLikes:likes.length,
+         totalViews: views
        }
 
        res.status(200).json(
-        new ApiResponse(201,stats,"Stats of the channel")
+        new ApiResponse(201,dashboard,"Stats of the channel")
        )
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
-    if(!req.user){
-       throw new ApiError(404,"Not found! You need to signin")
+   if (!req.user) {
+    throw new ApiError(404, "Not found! You need to signin");
+}
+
+const videos = await Video.aggregate([
+    {
+       $match: {
+          owner: req.user._id
+       }
+    },
+    {
+       $lookup: {
+          from: "likes",
+          foreignField: "video",
+          localField: "_id",
+          as: "likesDetails"
+       }
+    },
+    {
+        $addFields: {
+            likesCount: {
+                $size: "$likesDetails"
+            },
+            createdAt: {
+                $dateToString: {
+                    format: "%Y-%m-%d",
+                    date: "$createdAt"
+                }
+            },
+            day: {
+                $toInt: { $substr: ["$createdAt", 8, 2] }
+            },
+            month: {
+                $toInt: { $substr: ["$createdAt", 5, 2] }
+            },
+            year: {
+                $toInt: { $substr: ["$createdAt", 0, 4] }
+            }
+        }
     }
-   const allVideos = await Video.find({owner:req.user?._id})
-   
-       res.status(200).json(
-        new ApiResponse(
-            201,allVideos,"All the videos of the channel"
-        )
-       )
+]);
+
+res.status(200).json(
+    new ApiResponse(201, videos, "Stats of the channel")
+);
+
 })
 
 export {
